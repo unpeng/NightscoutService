ee321dd9bf29aec775c63329c39c0bf91249e771
Pete Schwamb <pete@schwamb.net>
2020-11-18 12:06:52 -0600

    Make onboarding support explicit

--- NightscoutServiceKitUI/NightscoutService+UI.swift
+++ NightscoutServiceKitUI/NightscoutService+UI.swift
@@ -16,6 +16,8 @@ extension NightscoutService: ServiceUI {
     public static var image: UIImage? {
         UIImage(named: "nightscout", in: Bundle(for: ServiceUICoordinator.self), compatibleWith: nil)!
     }
+    
+    public static var providesOnboarding: Bool { return true }
 
     public static func setupViewController(currentTherapySettings: TherapySettings, preferredGlucoseUnit: HKUnit, chartColors: ChartColorPalette, carbTintColor: Color, glucoseTintColor: Color, guidanceColors: GuidanceColors, insulinTintColor: Color) -> (UIViewController & ServiceSetupNotifying & CompletionNotifying)? {
         
