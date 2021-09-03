2d4982be6a21bf6afe76dd648ce29991040a11cc
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2021-09-03 17:03:33 -0400

    Remove dead code

--- NightscoutServiceKitUI/Views/ServiceStatusView.swift
+++ NightscoutServiceKitUI/Views/ServiceStatusView.swift
@@ -70,25 +70,3 @@ struct ServiceStatusView: View, HorizontalSizeClassOverride {
         }
     }
 }
-
-private struct RefreshOnAppearModifier<Tag: Hashable>: ViewModifier {
-    @State private var viewId = UUID()
-    @Binding var selection: Tag?
-    
-    func body(content: Content) -> some View {
-        content
-            .id(viewId)
-            .onAppear {
-                if selection != nil {
-                    viewId = UUID()
-                    selection = nil
-                }
-            }
-    }
-}
-
-private extension View {
-    func refreshOnAppear<Tag: Hashable>(selection: Binding<Tag?>? = nil) -> some View {
-        modifier(RefreshOnAppearModifier(selection: selection ?? .constant(nil)))
-    }
-}
